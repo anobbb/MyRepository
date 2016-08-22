9bf28072d3e77c562a87be0061bea034af775b23
starrysara7 <starrysara@naver.com>
2016-08-23 08:34:48 +0900

    게시판과제

--- JavaProgramming/src/ch05/exam03/BoardExample.java
+++ JavaProgramming/src/ch05/exam03/BoardExample.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class BoardExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
+		//String[][] boards = new String[100][];
 		String[][] boards = null;
 		boards = new String[100][];
 		
@@ -15,18 +16,22 @@ public class BoardExample {
 			System.out.println("---------------------------------------------------------------");
 			System.out.print("선택: ");
 			String choice = scanner.nextLine();
-			if(choice.equals("1")){
+			
+			if(choice.equals("1")){ //목록
 				System.out.println("***********************************************************");
 				System.out.println("번호\t제목\t\t글쓴이\t조회수");
 				System.out.println("***********************************************************");
-				for(String[] board : boards){
-					if(board != null){
-						System.out.println(board[0]+"\t"+board[1]+"\t\t"+board[3]+"\t"+board[4]);
+				if(boards != null){
+					for(String[] board : boards){
+						if(board != null){
+							System.out.println(board[0] + "\t"+board[1] + "\t\t" + board[3] + "\t" + board[4]);
+						}
 					}
 				}
 				
-			} else if(choice.equals("2")){
-				int view=0;
+				
+			} else if(choice.equals("2")){ //글쓰기
+				int view = 0;
 				System.out.print("제목입력: ");
 				String title = scanner.nextLine();
 				System.out.print("내용입력: ");
@@ -42,46 +47,47 @@ public class BoardExample {
 					
 				}
 				
-			} else if(choice.equals("3")){
+			} else if(choice.equals("3")){ //상세보기
+				System.out.print("선택한 글번호: ");
+				int bno = Integer.parseInt(scanner.nextLine());
 				System.out.println("************************************************************************");
 				System.out.println("번호\t제목\t내용\t\t글쓴이\t조회수");
 				System.out.println("************************************************************************");
-				for(String[] board : boards){
-					if(board != null){
-						int view = Integer.parseInt(board[4]);
-						++view;
-						board[4] = String.valueOf(view);
-						System.out.println(board[0] + "\t" + board[1] +"\t" + board[2] 
-								+ "\t\t" + board[3] + "\t" + board[4]);
+				//for(String[] board : boards){
+				String[] board = boards[bno];
+				if(boards != null){
+					int view = Integer.parseInt(board[bno]);
+					++view;
+					board[4] = String.valueOf(view);
+					System.out.println(board[0] + "\t" + board[1] +"\t" + board[2] 
+							+ "\t\t" + board[3] + "\t" + board[4]);
 					}
-				}
-			} else if(choice.equals("4")){
-				String bno = scanner.nextLine();
-				int no = Integer.parseInt(bno);
-				if(bno.equals(boards[no])){
-					System.out.print("제목입력: ");
-					String title = scanner.nextLine();
-					System.out.print("내용입력: ");
-					String content = scanner.nextLine();
-					for(int i=0; i<boards.length; i++){
-						if(boards[i] != null){
-							String[] board = { String.valueOf(i), title, content,  }; //글쓴이, 조회수 가져와서 넣기
-							boards[i] = board;
-							break;
+				//}
+			} else if(choice.equals("4")){ //수정
+				int bno = Integer.parseInt(scanner.nextLine());
+				for(String[] board : boards){
+					if(bno == Integer.parseInt(board[bno])){
+						System.out.print("제목입력: ");
+						String title = scanner.nextLine();
+						System.out.print("내용입력: ");
+						String content = scanner.nextLine();
+						for(int i=0; i<bno; i++){
+								board[1] = title; 
+								board[2] = content;
+								boards[i] = board;
+								break;
+							
 						}
 					}
 				}
 				
-			} else if(choice.equals("5")){
+				
+			} else if(choice.equals("5")){ //삭제
 				System.out.print("삭제할 글번호: ");
 				int bNum = Integer.parseInt(scanner.nextLine());
-				for(String[] board : boards){
-					if(board != null){
-						board[bNum] = null;
-					}
-				}
-				
-			} else if(choice.equals("6")){
+				boards[bNum] = null;
+			}
+			else if(choice.equals("6")){
 				break;
 			}
 		}
